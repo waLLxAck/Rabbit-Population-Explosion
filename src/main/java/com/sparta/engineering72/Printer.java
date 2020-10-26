5d742a19fb055090f8f82e1c26451cafcab36c42
Svilen Petrov <svilen.petrov97@gmail.com>
2020-10-26 17:08:31 +0000

    Time Span feature added

--- src/main/java/com/sparta/engineering72/Printer.java
+++ src/main/java/com/sparta/engineering72/Printer.java
@@ -4,4 +4,12 @@ public class Printer {
     public static void print(Object object) {
         System.out.println(object);
     }
+
+    public static void printEnterNumberPrompt() {
+        System.out.print("Enter a number of months: ");
+    }
+
+    public static void printError() {
+        System.err.println("Invalid input. Please enter a number with characters ranging from [0-9].");
+    }
 }
--- src/main/java/com/sparta/engineering72/Validation.java
+++ src/main/java/com/sparta/engineering72/Validation.java
@@ -1,5 +1,10 @@
 package com.sparta.engineering72;
 
+import java.util.Scanner;
+
 public class Validation {
 
+    public static boolean isValidNumber(Scanner input) {
+        return input.hasNextInt();
+    }
 }
