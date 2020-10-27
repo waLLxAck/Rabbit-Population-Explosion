fa6ffe5f4a5ab8fd74f609ba7cc6216b954aae6c
Svilen Petrov <svilen.petrov97@gmail.com>
2020-10-27 13:43:16 +0000

    InputCollector.java update; Added years

--- src/main/java/com/sparta/engineering72/Printer.java
+++ src/main/java/com/sparta/engineering72/Printer.java
@@ -1,38 +1,19 @@
 package com.sparta.engineering72;
 
 public class Printer {
-
     public static void print(Object object) {
         System.out.println(object);
     }
 
-    public static void printStartMessage() {
-        System.out.println("The Simulation will now begin");
-        System.out.println("All required user inputs should be entered in the console when prompted");
-        System.out.println("The final report from the simulation will also be displayed in the console");
+    public static void printEnterNumberPrompt() {
+        System.out.print("Enter a number: ");
     }
 
-    public static void printSimulationTime(int number) {
-        int years = number/12;
-        int months = number % 12;
-        System.out.printf("The Simulation ran for %d years and %d months", years, months);
+    public static void printError() {
+        System.err.println("Invalid input. Please enter a number with characters ranging from [0-9].");
     }
 
-    public static void printFinalPopulation(int number) {
-        System.out.println("The total population of Rabbits at the end of the simulation: " +number);
+    public static void printEnterNumberOf(String timeScale) {
+        System.out.print("Enter a number of "+timeScale.toLowerCase()+": ");
     }
-
-    public static void printMalePopulation(int number) {
-        System.out.println("The male population of Rabbits at the end of the simulation: " +number);
-    }
-
-    public static void printFemalePopulation(int number) {
-        System.out.println("The female population of Rabbits at the end of the simulation: " +number);
-    }
-
-    public static void printDeathCount(int number) {
-        System.out.println("The total number of Rabbits that died: " + number);
-    }
-
-
 }
