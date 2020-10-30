3ddcbd3bf5c17c58ce3bd3e78f5d0148123fa91c
Svilen Petrov <svilen.petrov97@gmail.com>
2020-10-30 09:22:30 +0000

    Refactoring Simulator v1.1; Added Application Choice

--- src/main/java/com/sparta/engineering72/Animal/Fox/FemaleFox.java
+++ src/main/java/com/sparta/engineering72/Animal/Fox/FemaleFox.java
@@ -47,28 +47,17 @@ public class FemaleFox extends Fox{
     public static List<Animal> breedFoxes(long count){
         final int averageOffspringCount = 5;
 
-        final List<BigInteger> listOfBigIntsMale = new ArrayList<>();
-        final List<BigInteger> listOfBigIntsFemale = new ArrayList<>();
-        int THREAD_COUNT = 10;
         List<Animal> animals = new ArrayList<>();
+
         MaleFox malefox = new MaleFox();
         FemaleFox femalefox = new FemaleFox();
-
-        List<Thread> threads = new ArrayList<>();
-        for (int i = 0; i < THREAD_COUNT; i++) {
-            listOfBigIntsMale.add(BigInteger.valueOf(0));
-        }
-        for (int i = 0; i < THREAD_COUNT; i++) {
-            listOfBigIntsFemale.add(BigInteger.valueOf(0));
-        }
 
         long[] randomGenders;
 
         if (count > Settings.MAX_COUNT_THRESHOLD){
             long totalOffspring = count*averageOffspringCount;
-            long totalOffspring2 = totalOffspring/2;
-            malefox.setCount(totalOffspring2);
-            femalefox.setCount(totalOffspring2);
+            malefox.setCount(totalOffspring/2);
+            femalefox.setCount(totalOffspring/2);
         } else {
             long countMaleOffspring = 0;
             long countFemaleOffspring = 0;
@@ -82,52 +71,6 @@ public class FemaleFox extends Fox{
             }
 
             randomGenders = Randomizer.getRandomGender(totalOffspring);
-
-//            for (int i = 0; i < THREAD_COUNT; i++) {
-//                final int k = i;
-//
-//                int lowerBound = (totalOffspring/THREAD_COUNT * i);
-//                int upperBound = (totalOffspring/THREAD_COUNT * (i+1));
-//                threads.add(new Thread(new Runnable() {
-//                    final int v = k;
-//                    @Override
-//                    public void run() {
-//                        int males = 0;
-//                        int females = 0;
-//                        for (int j = lowerBound; j < upperBound; j++) {
-//                            for (int i = 0; i < upperBound-lowerBound; i++) {
-//                                if (randomGenders[j] == 0) {
-//                                    males++;
-//                                } else {
-//                                    females++;
-//                                }
-//                            }
-//                        }
-//                        listOfBigIntsMale.set(v, BigInteger.valueOf(males));
-//                        listOfBigIntsFemale.set(v, BigInteger.valueOf(females));
-//                    }
-//                }));
-//            }
-//            for (Thread thread : threads) {
-//                thread.start();
-//            }
-//            for (Thread thread : threads) {
-//                try {
-//                    thread.join();
-//                } catch (InterruptedException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//            int sumOfMales = 0;
-//            int sumOfFemales = 0;
-//            for (BigInteger bigInt : listOfBigIntsMale) {
-//                sumOfMales += bigInt.intValue();
-//            }
-//            for (BigInteger bigInt : listOfBigIntsFemale) {
-//                sumOfFemales += bigInt.intValue();
-//            }
-//            malefox.setCount(sumOfMales);
-//            femalefox.setCount(sumOfFemales);
 
             for(int j = 0; j < totalOffspring; j++) {
                 if (randomGenders[j] == 1) {
